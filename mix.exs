c5f002bdd96bb3808e49d0cf4dd24cb2859492b1
Will Leyton <wleylop22@gmail.com>
2021-06-09 10:38:48 -0500

    Add Mix

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Genicon.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :genicon,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :crypto]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:egd, github: "erlang/egd"},
+      {:ex_doc, "~> 0.12"},
+      {:fe, "~> 0.1.2"},
+      {:monadex, "~> 1.1"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+end
